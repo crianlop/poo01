c0ec6478858252b5a3a0e82df83d2bd29c5ea296
mjcastro <mjcastro@espol.edu.ec>
2019-06-30 10:39:03 -0500

    Eliminacion de los getteres y setter de las clases de Empleado y Cliente ya que eran los mismos atributos de la clase Padre que es Usuario

--- SistemaBancario/src/main/java/Usuario/Cliente.java
+++ SistemaBancario/src/main/java/Usuario/Cliente.java
@@ -21,31 +21,7 @@ public class Cliente extends Usuario{
         
 
     }
-//    public void setNombre(String nombre_dado){
-//        nombre=nombre_dado;
-//    }
-//    public void setApellido(String apellido_dado){
-//        apellido=apellido_dado;
-//    }
-//    public void setUsuario(String usuario_dado){
-//        usuario=usuario_dado;
-//    }
-//    public void setContrasena(String contrasena_dada){
-//        contrasena=contrasena_dada;
-//    }
-//    public String getNombre(){
-//        return nombre;
-//    }
-//    public String getApellido(){
-//        return apellido;
-//    }
-//    public String getUsuario(){
-//        return usuario;
-//    }
-//    public String getContrasena(){
-//        return contrasena;
-//    }
-//            
+
 }
     
     
--- SistemaBancario/src/main/java/Usuario/Empleado.java
+++ SistemaBancario/src/main/java/Usuario/Empleado.java
@@ -19,37 +19,5 @@ public class Empleado extends Usuario{
         contrasena=Contrasena;
         ManejoArchivos.EscribirUsuarios(this, "E");
     }
-
-//    public String getNombre() {
-//        return nombre;
-//    }
-//
-//    public void setNombre(String nombre) {
-//        this.nombre = nombre;
-//    }
-//
-//    public String getApellido() {
-//        return apellido;
-//    }
-//
-//    public void setApellido(String apellido) {
-//        this.apellido = apellido;
-//    }
-//
-//    public String getUsuario() {
-//        return usuario;
-//    }
-//
-//    public void setUsuario(String usuario) {
-//        this.usuario = usuario;
-//    }
-//
-//    public String getContrasena() {
-//        return contrasena;
-//    }
-//
-//    public void setContrasena(String contrasena) {
-//        this.contrasena = contrasena;
-//    }
-    
+   
 }
